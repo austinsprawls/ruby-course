1324dea6be151c02f2a5fe38567200187df5f0f5
Austin Sprawls <austinsprawls@gmail.com>
2014-08-21 10:20:47 -0500

    implemented SignIn transaction script

--- lib/honkr/scripts/sign_in.rb
+++ lib/honkr/scripts/sign_in.rb
@@ -2,7 +2,20 @@ module Honkr
   class SignIn
 
     def self.run(params)
-      # TODO
+      user= Honkr.db.get_user_by_username(params[:username])
+
+      if user.has_password?(params[:password])
+        user_id = Honkr.db.create_session(:user_id => user.id)
+        return {
+          success?: true,
+          session_id: user_id
+        }
+      else
+        return {
+          success?: false,
+          error: :invalid_password
+        }
+      end
     end
 
   end
